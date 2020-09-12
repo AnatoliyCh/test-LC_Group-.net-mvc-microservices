7669b0219b5373a490576b76dfce02adeccc6f97
Anatoly <googlsocial97@gmail.com>
2020-09-13 06:33:24 +0700

    refactoring

--- test-LC_Group-.net-mvc-microservices/People/App_Data/PeopleRepository.cs
+++ test-LC_Group-.net-mvc-microservices/People/App_Data/PeopleRepository.cs
@@ -11,25 +11,12 @@ namespace PeopleModule.DataAccessLayer
         private const string URL_QUOTE = "https://geek-jokes.sameerkumar.website/api"; // цитата
         private const string PARENT_NODE_USER = "results[0]";
 
-        protected People MappingPeople(string jsonString)
-        {
-            People people = null;
-            var jObject = JObject.Parse(jsonString);
-            people = JsonSerializer.Deserialize<People>(jObject.SelectToken(PARENT_NODE_USER).ToString());
-            people.FirstName = (string)jObject.SelectToken(PARENT_NODE_USER + ".name.first");
-            people.LastName = (string)jObject.SelectToken(PARENT_NODE_USER + ".name.last");
-            people.City = (string)jObject.SelectToken(PARENT_NODE_USER + ".location.city");
-            people.Street = (string)jObject.SelectToken(PARENT_NODE_USER + ".location.street.name");
-            people.Picture = (string)jObject.SelectToken(PARENT_NODE_USER + ".picture.medium");
-            return people;
-        }
+        private readonly PeopleService peopleService = new PeopleService();
+        
         protected void SetQuote(People people, HttpClient client)
         {
             if (people == null || client == null) return;
-            var response = client.GetAsync(URL_QUOTE);
-            response.Wait();
-
-            var result = response.Result;
+            var result = PeopleService.ResponseResult(URL_QUOTE, client); // получение quote
             if (result.IsSuccessStatusCode) people.Quote = result.Content.ReadAsStringAsync().Result.Replace("\"", "").Replace("\n", "");
         }
 
@@ -37,16 +24,12 @@ namespace PeopleModule.DataAccessLayer
         {
             People newPeople = null;
             using (var client = new HttpClient())
-            {
-                // получение randomuser
-                var responseUser = client.GetAsync(URL_USER);
-                responseUser.Wait();
-
-                var resultUser = responseUser.Result;
+            {                
+                var resultUser = PeopleService.ResponseResult(URL_USER, client); // получение randomuser
                 if (resultUser.IsSuccessStatusCode)
                 {
-                    newPeople = MappingPeople(resultUser.Content.ReadAsStringAsync().Result); // e
-                    SetQuote(newPeople, client); // получение quote
+                    newPeople = peopleService.MappingPeople(resultUser.Content.ReadAsStringAsync().Result, PARENT_NODE_USER);
+                    SetQuote(newPeople, client);
                 }
             }
             return newPeople;
---
+++ test-LC_Group-.net-mvc-microservices/People/App_Data/PeopleService.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using PeopleModule.Models;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace PeopleModule.DataAccessLayer
+{
+    public class PeopleService
+    {
+        /// <summary> Возвращает результат запроса </summary>
+        /// <param name="url">ссылка для запроса</param>
+        /// <param name="client">внешний HttpClient</param>
+        /// <returns>результат запроса</returns>
+        public static HttpResponseMessage ResponseResult(string url, HttpClient client)
+        {
+            var response = client.GetAsync(url);
+            response.Wait();
+            return response.Result;
+        }
+
+        /// <summary> Конвертирование JSON в People </summary>
+        /// <param name="jsonString">JSON</param>
+        /// <param name="parentNode">базовый узел JSON</param>
+        /// <returns>object || null</returns>
+        public People MappingPeople(string jsonString, string parentNode)
+        {
+            People people = null;
+            var jObject = JObject.Parse(jsonString);
+            people = JsonSerializer.Deserialize<People>(jObject.SelectToken(parentNode).ToString());
+            people.FirstName = (string)jObject.SelectToken(parentNode + ".name.first");
+            people.LastName = (string)jObject.SelectToken(parentNode + ".name.last");
+            people.City = (string)jObject.SelectToken(parentNode + ".location.city");
+            people.Street = (string)jObject.SelectToken(parentNode + ".location.street.name");
+            people.Picture = (string)jObject.SelectToken(parentNode + ".picture.medium");
+            return people;
+        }        
+    }
+}
