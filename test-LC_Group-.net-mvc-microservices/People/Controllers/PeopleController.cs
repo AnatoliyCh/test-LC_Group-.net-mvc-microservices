ca5ca3cd543e6b2aa046a85b32c1553c6820d35a
Anatoly <googlsocial97@gmail.com>
2020-09-13 01:24:12 +0700

    begin api/people/getInfo

---
+++ test-LC_Group-.net-mvc-microservices/People/Controllers/PeopleController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace People.Controllers
+{
+    public class PeopleController : ApiController
+    {
+        [HttpGet]
+        [Route("api/people/getInfo")]
+        public IHttpActionResult GetInfo()
+        {
+            return Ok("123");
+        }
+    }
+}
---
+++ test-LC_Group-.net-mvc-microservices/People/Models/People.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace People.Models
+{
+    public class People
+    {
+        public string Gender { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Email { get; set; }
+        public string Picture { get; set; }
+        public string Poem { get; set; }
+        public string Quote { get; set; }
+        public string Distance { get; set; }
+    }
+}
