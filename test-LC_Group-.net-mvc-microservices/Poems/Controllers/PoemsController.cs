2a3b3a3f1c30df7b56838e4d5fd0b47cdd8d4655
Anatoly <googlsocial97@gmail.com>
2020-09-13 07:59:47 +0700

    PoemsController

---
+++ test-LC_Group-.net-mvc-microservices/Poems/Controllers/PoemsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PoemsModule.Controllers
+{
+    public class PoemsController : ApiController
+    {
+        [HttpGet]
+        [Route("api/poems/getPoem/{id}")]
+        public IHttpActionResult GetPoem(int id)
+        {            
+            return Ok();
+        }
+    }
+}
