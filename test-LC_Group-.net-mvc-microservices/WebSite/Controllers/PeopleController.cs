4807c0b3fc23b7e47c4530cfb72332b9964b5223
Anatoly <googlsocial97@gmail.com>
2020-09-14 05:06:24 +0700

    init

---
+++ test-LC_Group-.net-mvc-microservices/WebSite/Controllers/PeopleController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PeopleModule.DataAccessLayer;
+using PeopleModule.Models;
+
+namespace WebSite.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private PeopleContext db = new PeopleContext();
+
+        // GET: People
+        public ActionResult Index()
+        {
+            return View(db.Peoples.ToList());
+        }
+
+        // GET: People/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            People people = db.Peoples.Find(id);
+            if (people == null)
+            {
+                return HttpNotFound();
+            }
+            return View(people);
+        }
+
+        // GET: People/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: People/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Gender,FirstName,LastName,City,Street,Email,Picture,Poem,Quote,Distance")] People people)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Peoples.Add(people);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(people);
+        }
+
+        // GET: People/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            People people = db.Peoples.Find(id);
+            if (people == null)
+            {
+                return HttpNotFound();
+            }
+            return View(people);
+        }
+
+        // POST: People/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Gender,FirstName,LastName,City,Street,Email,Picture,Poem,Quote,Distance")] People people)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(people).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(people);
+        }
+
+        // GET: People/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            People people = db.Peoples.Find(id);
+            if (people == null)
+            {
+                return HttpNotFound();
+            }
+            return View(people);
+        }
+
+        // POST: People/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            People people = db.Peoples.Find(id);
+            db.Peoples.Remove(people);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
